62b719c2472e2e801511c7fae33100df3e92192b
yiwen song <songzgy@gmail.com>
2016-04-04 19:20:50 -0700

    debug

--- kmer_hash_upc.h
+++ kmer_hash_upc.h
@@ -20,7 +20,7 @@ void hex_dump(void* ptr,size_t size)
   char *p = (char*) ptr;
   for(size_t i=0;i<size;i++)
   {
-    if(i % 16) sprintf(s+3*i,"%x ",*((char*)p) & 0xff);
+    if(i % 16 - 15) sprintf(s+3*i,"%x ",*((char*)p) & 0xff);
     else sprintf(s+3*i,"%x\n",*((char*)p) & 0xff);
   }
   fputs(s,stderr);
