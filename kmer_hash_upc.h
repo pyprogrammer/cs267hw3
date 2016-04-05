d1a2425cbaeda1dbc0e7701520a31c841079f4be
yiwen song <songzgy@gmail.com>
2016-04-04 20:02:20 -0700

    debug

--- kmer_hash_upc.h
+++ kmer_hash_upc.h
@@ -21,7 +21,7 @@ void hex_dump(void* ptr,size_t size)
   fprintf(stderr,"size: %d\n",size);
   for(size_t i=0;i<size;i++)
   {
-    fprintf(stderr,"WTF IS THE I %d AND THE CHAR %c\n",i,p[i]);
+    fprintf(stderr,"%hhx ",p[i] & 0xff);
     sprintf(s+3*i,"%hhx ",p[i] & 0xff);
   }
   s[size*3] = '\n';
